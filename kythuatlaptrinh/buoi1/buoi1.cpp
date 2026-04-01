e4bd91b07cbd5e92dd9794cc2cdb9179785c5490
Admin <Admin@ADMIN-PC>
2026-04-01 11:55:04 +0700

    xong buoi1

--- kythuatlaptrinh/buoi1/buoi1.cpp
+++ kythuatlaptrinh/buoi1/buoi1.cpp
@@ -1,20 +1,23 @@
-// buoi1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// buoi1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
-
+#include<vector>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    // tạo mảng động lưu điểm
+    vector <float> diem;
+    // thêm điểm 5 người
+	diem.push_back(20);
+	diem.push_back(10);
+	diem.push_back(13);
+	diem.push_back(6);
+	diem.push_back(9);	
+	// xoá điểm của người cuối
+	diem.pop_back();
+	// duyệt qua mảng để in ra màn hình
+	for (int i = 0; i < diem.size(); i++) {
+		cout << diem[i] << endl;
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
